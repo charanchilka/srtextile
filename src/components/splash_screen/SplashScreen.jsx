9ff2a190d5604063ae6dfd289edb03b942f07f05
charan <charanrajc@fermion.in>
2021-06-19 12:52:22 +0530

    initial commit

---
+++ src/components/splash_screen/SplashScreen.jsx
@@ -0,0 +1,12 @@
+import './SplashScreen.css';
+
+function SplashScreen(){
+ 
+    return(
+        <div className="main_background">
+            Hello World
+        </div>
+    )
+}
+
+export default SplashScreen;
