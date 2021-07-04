237a4d21084eaf9f86566136c00f3a9b0b9828eb
k.poonam <poonam@ilmux.com>
2021-07-04 17:06:35 +0530

    responsive

--- src/components/about_us/AboutUs.jsx
+++ src/components/about_us/AboutUs.jsx
@@ -6,20 +6,22 @@ const Header = () => {
     return (
         <div className="about-us-main">
             <div className="row no-gutters align-items-center">
-                <div className="col-6 text-right">
+                <div className="col-lg-6 col-12 col-md-12 col-sm-12 text-right">
                     <div className="about-towel">
                         <img className="image-position" src={Towels} alt="towels" />
                     </div>
                 </div>
-                <div className="col-6">
+                <div className="col-lg-6 col-12 col-md-12 col-sm-12">
                     <div className="about-content">
                         <h3>About Us</h3>
                         <div className="row no-gutters">
-                            <div className="col-11 no-gutters">
+                            <div className="col-lg-11 col-12 col-md-12 col-sm-12 no-gutters">
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Condimentum diam orci pretium a pharetra, feugiat cursus. Dictumst risus, sem egestas odio cras adipiscing vulputate. Nisi, risus in suscipit non. Non commodo volutpat, pharetra, vel.</p>
                             </div>
                         </div>
+                        <div className="read-more">
                         <button className="button-style"> Read More <span> <img src={Arrow} alt="arrow" /></span></button>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/featured_collection/Collection.jsx
+++ src/components/featured_collection/Collection.jsx
@@ -1,32 +1,38 @@
-import './Collection.css';
-import CollectionImage from '../../Assets/Images/image001.JPG'
+import "./Collection.css";
+import CollectionImage from "../../Assets/Images/image001.JPG";
 
 const FeaturedCollection = () => {
-
-    return (
-        <div className="main-collection">
-            <div className="row no-gutters">
-                <div className="col-6">
-                    <div className="row no-gutters featured-collection d-flex justify-content-center align-items-center">
-                        Featured Collection
-                    </div>
-                    <div className="row no-gutters customized-goods">
-                        Customized Goods
-                    </div>
-                    <div className="row no-gutters text-content">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer.
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer.
-                    </div>
-                    <div className="row no-gutters">
-                        <button className="view-button">View More</button>
-                    </div>
-                </div>
-                <div className="col-6 d-flex justify-content-center align-items-center">
-                    <img className="img-style" src={CollectionImage} alt="collection img"/>
-                </div>
+  return (
+    <div className="main-collection">
+      <div className="row no-gutters">
+        <div className="col-12 col-md-12 col-lg-6 col-sm-12">
+          <div className="row no-gutters">
+            <div className="col-lg-2 col-12 col-md-12 col-sm-12"></div>
+            <div className="col-lg-9 col-12 col-md-12 col-sm-12">
+              <p className="featured-collection">Featured Collection</p>
+              <div className="customized-goods">Customized Goods</div>
+              <div className="text-content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                odio in et, lectus sit lorem id integer. Lorem ipsum dolor sit
+                amet, consectetur adipiscing elit. Nunc odio in et, lectus sit
+                lorem id integer.
+              </div>
+              <div className="view-button-container">
+                <button className="view-button">View More</button>
+              </div>
             </div>
+          </div>
         </div>
-    )
-}
+        <div className="col-12 col-md-12 col-lg-6 col-sm-12 d-flex align-items-center">
+          <img
+            className="img-style"
+            src={CollectionImage}
+            alt="collection img"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default FeaturedCollection;
