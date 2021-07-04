d2cec0684c99672e8cd530de92eb1a682464a3ff
charan <charanrajc@fermion.in>
2021-07-04 20:00:19 +0530

    Reach us page implemented

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -4,6 +4,8 @@ import Footer from '../footer/Footer';
 import AboutUs from '../about_us/AboutUs';
 import Collection from '../featured_collection/Collection';
 import GetDeal from '../get_a_deal/GetADeal';
+import GetInTouchSub from '../get_in_touch/GetInTouchSub';
+import ReachUsMap from '../reach_us/ReachUsMap';
 
 const Dashboard = () =>{
     return(
@@ -12,6 +14,8 @@ const Dashboard = () =>{
             <Collection/>
             <AboutUs/>
             <GetDeal/>
+            <GetInTouchSub/>
+            <ReachUsMap/>
             <Footer/>
         </div>
     )
