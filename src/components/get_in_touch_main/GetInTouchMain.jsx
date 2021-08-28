b60a0d38d37c3c2f4a0f1f77b536070ccd75d0e4
k.poonam <poonam@ilmux.com>
2021-08-28 18:27:31 +0530

    routing and get in touch

--- src/components/get_in_touch_main/GetInTouchMain.jsx
+++ src/components/get_in_touch_main/GetInTouchMain.jsx
@@ -1,27 +1,27 @@
-import Header from '../header/Header';
-import Footer from '../footer/Footer'
-  import './GetInTouchMain.css';  
-  import React from "react";
-  import BackgroundTouch from '../../Assets/Images/getInTouchBack.svg'
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
+import "./GetInTouchMain.css";
+import React from "react";
+import BackgroundTouch from "../../Assets/Images/getInTouchBack.svg";
 
-  class GetInTouchMain extends React.Component {
-    state = {
-      navBackground: "transparent",
-    };
-  
-    componentDidMount() {
-      document.addEventListener("scroll", () => {
-        const backgroundcolor = window.scrollY < 500 ? "transparent" : "bold";
-        this.setState({ navBackground: backgroundcolor });
-      });
-    }
-  
-    render() {
-    return(
+class GetInTouchMain extends React.Component {
+  state = {
+    navBackground: "transparent",
+  };
+
+  componentDidMount() {
+    document.addEventListener("scroll", () => {
+      const backgroundcolor = window.scrollY < 500 ? "transparent" : "bold";
+      this.setState({ navBackground: backgroundcolor });
+    });
+  }
+
+  render() {
+    return (
       <div>
-            <Header background={this.state} />
+        <Header background={this.state} />
         <div className="getInTouchMainContainer">
-              <img src={BackgroundTouch} alt=""/>
+          <img src={BackgroundTouch} alt="" />
           <div className="contactForm">
             <div className="headerForm">
               Get in <span className="touchCss">Touch</span> with us
@@ -30,47 +30,69 @@ import Footer from '../footer/Footer'
               <div className="row g-0">
                 <div className="col-12">
                   <div className="row g-0">
-                    <div className="col-1">
-                    <i class="far fa-user"></i>
+                    <div className="col-1 d-flex justify-content-center align-items-center">
+                      <i className="far fa-user"></i>
                     </div>
                     <div className="col-11">
-                    <div class="form-group">
-                    <label for="usr">Name</label>
-                    <input type="text" class="form-control" id="usr"/>
-                  </div>
+                      <div className="form-group">
+                        <label>Name</label>
+                        <input type="text" className="form-control" id="name" />
+                      </div>
                     </div>
                   </div>
-                 
                 </div>
                 <div className="col-12">
-                <div class="form-group">
-                    <label for="usr">Email ID</label>
-                    <input type="email" class="form-control" id="usr"/>
+                  <div className="row g-0">
+                    <div className="col-1 d-flex justify-content-center align-items-center">
+                      <i className="far fa-envelope"></i>
+                    </div>
+                    <div className="col-11">
+                      <div className="form-group">
+                        <label>Email ID</label>
+                        <input type="email" className="form-control" id="emailid" />
+                      </div>
+                    </div>
                   </div>
                 </div>
                 <div className="col-12">
-                <div class="form-group">
-                    <label for="usr">Phone No.</label>
-                    <input type="tel" class="form-control" id="usr"/>
+                  <div className="row g-0">
+                    <div className="col-1 d-flex justify-content-center align-items-center">
+                    <i className="fas fa-phone-alt"></i>
+                    </div>
+                    <div className="col-11">
+                      <div className="form-group">
+                        <label>Phone No.</label>
+                        <input type="tel" className="form-control" id="phoneno" />
+                      </div>
+                    </div>
                   </div>
                 </div>
                 <div className="col-12">
-                <div class="form-group">
-                    <label for="usr">Message:</label>
-                    <textarea  rows="2" class="form-control" id="usr"></textarea>
+                  <div className="row g-0">
+                    <div className="col-1 d-flex justify-content-center align-items-center">
+                    <i className="far fa-comment-alt"></i>
+                    </div>
+                    <div className="col-11">
+                      <div className="form-group">
+                        <label>Message:</label>
+                        <textarea
+                          rows="2"
+                          className="form-control"
+                          id="comment"
+                        ></textarea>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="submitBtn">
-              Submit
-            </div>
+            <div className="submitBtn">Submit</div>
           </div>
         </div>
-            <Footer />
+        <Footer />
       </div>
-    )
-    }
+    );
   }
+}
 
-  export default GetInTouchMain;
+export default GetInTouchMain;
