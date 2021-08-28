3792568d1156e92f7220ca056ad565183d4eb1aa
charan <charanrajc@fermion.in>
2021-08-28 22:12:05 +0530

    app changes 02

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const GetInTouchMain = lazy(
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="https://charanchilka.github.io/srtextile/">
+      <BrowserRouter basename="/srtextile">
         <Suspense fallback={<div />}>
           <Switch>
             <Route exact path="/" component={Dashboard} />
